5caef067b250bde8f3389ef4367c4f200d903622
Siphesihle Mnyengeza <Siphesihle.Mnyengeza@bbd.co.za>
2025-02-03 20:43:56 +0200

    Reveerting

--- src/index.js
+++ src/index.js
@@ -1,44 +1,46 @@
-  const myLocation = document.getElementById("myLocation");
-        const weatherElement = document.getElementById("weather");
+const myLocation = document.getElementById("myLocation");
 
-        function fetchLocation() {
-            fetch("https://api.ipify.org?format=json")
-                .then(response => response.json())
-                .then(data => {
-                    let userIP = data.ip;
-                    return fetch(`http://ip-api.com/json/${userIP}?fields=status,message,city,countryCode`);
-                })
-                .then(response => response.json())
-                .then(locationData => {
-                    if (locationData.status !== "success") {
-                        myLocation.innerHTML = "Location not found.";
-                        return;
-                    }
-                    myLocation.innerHTML = `You are at: ${locationData.city}, ${locationData.countryCode}`;
-                })
-                .catch(error => {
-                    console.error("Error fetching location:", error);
-                    myLocation.innerHTML = "Unable to get location.";
-                });
-        }
-        function fetchWeather() {
-            const apiKey = "2e0f0affa2f1dffc3136bd15e05a3af8";
-            const city = "Johannesburg";
+var endpoint = 'http://ip-api.com/json/24.48.0.1?fields=status,message,city';
 
-            fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`)
-                .then(response => response.json())
-                .then(weatherData => {
-                    if (weatherData.cod !== 200) {
-                        weatherElement.innerHTML = "Weather data not available.";
-                        return;
-                    }
-                    weatherElement.innerHTML = `Weather in ${weatherData.name}: ${weatherData.weather[0].description}, ${weatherData.main.temp}°C`;
-                })
-                .catch(error => {
-                    console.error("Error fetching weather:", error);
-                    weatherElement.innerHTML = "Unable to get weather.";
-                });
+var xhr = new XMLHttpRequest();
+xhr.onreadystatechange = function () {
+    if (this.readyState === 4 && this.status === 200) {
+        var response = JSON.parse(this.responseText);
+        if (response.status !== 'success') {
+            console.log('query failed: ' + response.message);
+            return
         }
 
-        fetchLocation();
-        fetchWeather();
+        myLocation.innerHTML = "You are at: " + response.city + ", " + response.countryCode;
+
+        if (myLocation.countryCode == "US") {
+           window.location.href = "https://www.google.com";
+        } else if (myLocation.countryCode == "CA") {
+            window.location.href = "https://www.google.com";
+        }
+
+        getWeather(response.city);
+    }
+};
+xhr.open('GET', endpoint, true);
+xhr.send();
+
+function getWeather(city ) {
+    const apiKey = "2e0f0affa2f1dffc3136bd15e05a3af8";
+let weatherEndpoint = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`;
+
+fetch(weatherEndpoint)
+    .then((response) => response.json())
+    .then((data) => {
+        console.log(data);
+        let temperature = data.main.temp;
+        let description = data.weather[0].description;
+        let icon = data.weather[0].icon;
+        let iconUrl = `http://openweathermap.org/img/w/${icon}.png`;
+
+        weather.innerHTML = `It is currently ${temperature} degrees with ${description} in ${city}. <img src="${iconUrl}" alt="weather icon">`;
+    })
+    .catch((error) => {
+        console.log(error);
+    });
+}
