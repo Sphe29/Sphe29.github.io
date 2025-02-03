c79754225c1384f96fb0871bf28979a7557b67f7
Siphesihle Mnyengeza <Siphesihle.Mnyengeza@bbd.co.za>
2025-02-03 19:11:26 +0200

    Location Feature

---
+++ src/index.js
@@ -0,0 +1,26 @@
+const random = document.getElementById("location");
+
+var endpoint = 'http://ip-api.com/json/24.48.0.1?fields=status,message,city';
+
+var xhr = new XMLHttpRequest();
+xhr.onreadystatechange = function() {
+	if (this.readyState == 4 && this.status == 200) {
+		var response = JSON.parse(this.responseText);
+		if(response.status !== 'success') {
+			console.log('query failed: ' + response.message);
+			return
+		}
+		if(response.countryCode == "US") {
+			window.random.replace("https://google.com/");
+		}
+		if(response.countryCode == "CA") {
+			window.random.replace("https://google.ca/");
+		}
+	}
+};
+xhr.open('GET', endpoint, true);
+xhr.send();
+
+// let weather = document.getElementById("weather");
+// let weatherEndpoint = `https://api.openweathermap.org/data/2.5/weather?q={Johannesburg}&appid={2e0f0affa2f1dffc3136bd15e05a3af8}}`
+// document.getElementById("name").innerHTML = "Johannesburg";
